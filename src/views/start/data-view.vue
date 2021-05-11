cbe4a8f93e6b4ffdfa5985455e3c1b4b1c71c47c
小明 <531431988@qq.com>
2021-05-11 15:35:51 +0800

    更新切换场景

--- src/views/start/data-view.vue
+++ src/views/start/data-view.vue
@@ -6,58 +6,76 @@
 import 'pepjs'
 import * as BABYLON from 'babylonjs'
 import * as GUI from 'babylonjs-gui'
+import 'babylonjs-inspector'
 import 'babylonjs-loaders'
 export default {
+  data () {
+    return {
+      scene: 1
+    }
+  },
   methods: {
     createScene (canvas) {
       const engine = new BABYLON.Engine(canvas, true)
-      var scene = new BABYLON.Scene(engine)
-      var camera = new BABYLON.ArcRotateCamera('Camera', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene)
-      camera.setPosition(new BABYLON.Vector3(0, 0, 10))
-      camera.attachControl(canvas, true)
-      // camera.lowerRadiusLimit = 100
-      // camera.upperRadiusLimit = 10
-
-      BABYLON.SceneLoader.Append('./models/gltf/tap/', 'scene.gltf', scene)
+      this.loadScene({ engine, canvas, url: './models/gltf/dongfangnanfang/', name: 'dfnf.gltf', text: '进入1层' })
+    },
+    // 动态加载场景
+    loadScene ({ engine, canvas, url, name, text }) {
+      const scene = new BABYLON.Scene(engine)
+      var camera = null
+      var light = null
+      if (text === '进入1层') {
+        camera = new BABYLON.ArcRotateCamera('Camera', 4.67, 1.45, 138, new BABYLON.Vector3(0, -100, 50), scene)
+        camera.lowerBetaLimit = 0.1 // 纬度轴上允许的最小角度
+        camera.upperBetaLimit = (Math.PI / 2) * 0.9 // 纬度轴上允许的最大角度
+        camera.lowerRadiusLimit = 100 // 摄像机到目标的最小允许距离（摄像机无法靠近）。
+        camera.upperRadiusLimit = 200 // 摄像机到目标的最大允许距离（摄像机不能再远了）。
+        camera.attachControl(canvas, true)
+        light = new BABYLON.DirectionalLight('DirectionalLight', new BABYLON.Vector3(0.60, -0.7, 0.63), scene)
+        light.position = new BABYLON.Vector3(-0.05, 0.35, -0.05)
+        light.intensity = 8
+        new BABYLON.HemisphericLight('HemiLight', new BABYLON.Vector3(0, 1, 0), scene)
+      } else {
+        camera = new BABYLON.ArcRotateCamera('Camera', 4.66, 1.15, 100, new BABYLON.Vector3(0, -100, 50), scene)
+        /* camera.lowerBetaLimit = 0.1 // 纬度轴上允许的最小角度
+        camera.upperBetaLimit = (Math.PI / 2) * 0.9 // 纬度轴上允许的最大角度
+        camera.lowerRadiusLimit = 100 // 摄像机到目标的最小允许距离（摄像机无法靠近）。
+        camera.upperRadiusLimit = 200 // 摄像机到目标的最大允许距离（摄像机不能再远了）。
+        camera.attachControl(canvas, true)
+        light = new BABYLON.DirectionalLight('DirectionalLight', new BABYLON.Vector3(0.60, -0.7, 0.63), scene)
+        light.position = new BABYLON.Vector3(-0.05, 0.35, -0.05)
+        light.intensity = 8
+        new BABYLON.HemisphericLight('HemiLight', new BABYLON.Vector3(0, 1, 0), scene) */
+      }
+      BABYLON.SceneLoader.Append(url, name, scene, res => {
+        this.createGUI({ engine, canvas, text })
+      }, () => { }, error => {
+        console.log('error', error)
+      })
       engine.runRenderLoop(() => {
         scene.render()
       })
       window.addEventListener('resize', function () {
         engine.resize()
       })
-      console.log(scene)
-      this.createGUI(scene)
+      scene.debugLayer.show()
     },
-    createGUI (scene) {
-      const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI('UI')
-      var button1 = new GUI.Button.CreateSimpleButton('but1', '1')
-      button1.top = '-20%'
-      button1.width = '40px'
+    createGUI ({ engine, canvas, text = '1' }) {
+      var advancedTexture = new GUI.AdvancedDynamicTexture.CreateFullscreenUI('enter-the-buliding')
+      var button1 = new GUI.Button.CreateSimpleButton('btn', text)
+      button1.width = '100px'
       button1.height = '40px'
-      button1.color = 'white'
-      button1.cornerRadius = 1000
+      button1.left = '13%'
+      button1.color = '#fff'
       button1.background = 'rgba(0,0,0,.5)'
-      button1.onPointerUpObservable.add(function () {
-        alert(11111)
+      button1.onPointerUpObservable.add(() => {
+        if (text === '进入1层') {
+          this.loadScene({ engine, canvas, url: './models/gltf/first-floor/', name: '1c.gltf', text: '全景' })
+        } else {
+          this.loadScene({ engine, canvas, url: './models/gltf/dongfangnanfang/', name: 'dfnf.gltf', text: '进入1层' })
+        }
       })
       advancedTexture.addControl(button1)
-      button1.linkWithMesh(scene.meshes[0])
-
-      /* const btn = new GUI.Ellipse('btn')
-      const text = new GUI.TextBlock()
-      text.text = '1'
-      btn.width = '40px'
-      btn.height = '40px'
-      btn.color = '#fff'
-      btn.thickness = 2
-      btn.background = 'rgba(0,0,0,.5)'
-      btn.addControl(text)
-      btn.onPointerClickObservable.add(function () {
-        alert('you did it!')
-      })
-      advancedTexture.addControl(btn)
-      btn.linkWithMesh(scene.meshes[0])
-      console.log(btn) */
     },
     refreshScale () {
       const baseWidth = document.documentElement.clientWidth
@@ -84,5 +102,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+#actionTabs {
+  input,
+  button,
+  select,
+  optgroup,
+  textarea {
+    color: #333;
+  }
+}
 </style>
